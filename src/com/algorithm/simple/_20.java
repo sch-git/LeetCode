86a6c863a236d7561c18d5be05266c77e9f11843
sch <1208579668@qq.com>
2019-12-17 21:13:33 +0800

    LeetCode_20:有效的括号

--- src/com/algorithm/simple/_20.java
+++ src/com/algorithm/simple/_20.java
@@ -1,9 +1,41 @@
 package com.algorithm.simple;
 
+import java.util.Stack;
+
 /**
- * @Description: TODO
+ * @Title: 有效的括号
+ * @Description: 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 有效字符串需满足：左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
  * @Author: chenghao.su
- * @Date: 2019/12/17 20:38
+ * @Date: 2019/12/17 20:41
  */
 public class _20 {
+    public static void main(String[] args) {
+        System.out.println(isValid("]"));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int len = s.length();
+        // 将字符串压入栈中
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (!stack.isEmpty()) {
+                char stackCh = stack.pop();
+                if (ch - stackCh < 1 || ch - stackCh > 2) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        if (!stack.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
 }
