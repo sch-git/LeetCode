48cd6606cfe6f5d109753f63b1cafc114b78cad2
sch <1208579668@qq.com>
2020-01-27 12:48:56 +0800

    LeetCode_198：打家劫舍

---
+++ src/com/algorithm/simple/_198.java
@@ -0,0 +1,31 @@
+package com.algorithm.simple;
+
+/**
+ * @Title: 打家劫舍
+ * @Description: 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+ * @Author: chenghao.su
+ * @Date: 2020/1/27 12:38
+ */
+public class _198 {
+    public static void main(String[] args) {
+
+    }
+
+    public int rob(int[] nums) {
+        if (nums.length < 1) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        int max = 0;
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
+}
