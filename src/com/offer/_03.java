f8088d436314e4837f0ad025ebd5ad37c944840f
sch <1208579668@qq.com>
2020-02-29 09:39:16 +0800

    剑指offer_03：数组中重复的数字

---
+++ src/com/offer/_03.java
@@ -0,0 +1,44 @@
+package com.offer;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * @Solution: hash/排序比较
+ * @Title: 数组中重复的数字
+ * @Description: 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author: chenghao.su
+ * @Date: 2020/2/29 09:29
+ */
+public class _03 {
+    public static void main(String[] args) {
+
+    }
+
+    public int findRepeatNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    // 使用hash
+    public int findRepeatNumber2(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) == null) {
+                map.put(nums[i], nums[i]);
+            } else {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
