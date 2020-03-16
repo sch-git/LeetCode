66c6f645cee85180fd037e16a6fe775907d6a379
sch <1208579668@qq.com>
2020-03-16 14:48:26 +0800

    剑指offer_09：用两个栈实现队列

---
+++ src/com/offer/_09.java
@@ -0,0 +1,46 @@
+package com.offer;
+
+import java.util.Stack;
+
+/**
+ * @Solution:
+ * @Title: 用两个栈实现队列
+ * @Description: 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author: chenghao.su
+ * @Date: 2020/3/16 14:13
+ */
+public class _09 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class CQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            return -1;
+        }
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
