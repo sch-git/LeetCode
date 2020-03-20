38276303280171cf4c8d29c314ad5af20f9c335b
sch <1208579668@qq.com>
2020-03-20 16:04:24 +0800

    快速排序

--- src/com/sort/QuickSort.java
+++ src/com/sort/QuickSort.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  */
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arr = new int[]{3, 4, 2, 1, 5, 6, 7, 8, 30, 50, 1, 33, 24, 5, -4, 7, 0};
+        int[] arr = new int[]{5, 4, 1, 3, 2};
         quick(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
@@ -60,5 +60,41 @@ public class QuickSort {
         arr[start] = pivot;
         arr[left] = temp;
         return start;
+    }
+
+    /**
+     * 支点取中，L，R为最小，最大位置
+     * 循环比较，L比支点大，R比支点小，交换
+     * 递归L-支点-1，支点+1-R
+     *
+     * @param arr 待排序数组
+     * @param L   起始位置
+     * @param R   终点位置
+     */
+    public static void sort(int[] arr, int L, int R) {
+        int left = L;
+        int right = R;
+        int index = arr[(L + R) / 2];
+        while (left <= right) {
+            while (index > arr[left] && left < right) {
+                left++;
+            }
+            while (index < arr[right] && left < right) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        if (L < right) {
+            sort(arr, L, right);
+        }
+        if (left < R) {
+            sort(arr, left, R);
+        }
     }
 }
